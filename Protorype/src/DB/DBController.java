05eeaa572a87f2bdb04766ebf8fe2ab8d313665a
leonikoboy@gmail.com <ADMIN@DESKTOP-I4CLPDV>
2025-12-03 11:38:40 +0200

    .

--- Protorype/src/DB/DBController.java
+++ Protorype/src/DB/DBController.java
@@ -1,5 +1,35 @@
-package src;
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+
 
 public class DBController {
 
-}
+
+	    private static Connection conn = null;
+
+	    // connect to data_base
+	    public static Connection getConnection() {
+	        if (conn == null) {
+	            try {
+	                
+	                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/order?serverTimezone=Asia/Jerusalem&useSSL=false","root","Root1234");
+	                System.out.println("SQL connection initialized");
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        return conn;
+	    }
+
+	
+	}
+
+
+
+
