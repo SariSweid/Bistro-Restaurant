ba52e5815e9d32c824edafc2c8901ea70f64ffa8
leonikoboy@gmail.com <ADMIN@DESKTOP-I4CLPDV>
2025-12-05 15:51:14 +0200

    javafx problem

--- Protorype/src/gui/ServerApp.java
+++ Protorype/src/gui/ServerApp.java
@@ -39,7 +39,7 @@ public class ServerApp extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         launch(args);
     }
 }
