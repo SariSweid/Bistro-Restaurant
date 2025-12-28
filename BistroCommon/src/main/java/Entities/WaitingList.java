0e4f62a58f6379916f86ba724738795fbb5172ef
yarin8294 <yarham1999@gmail.com>
2025-12-28 16:44:46 +0200

    added WaitingList entity

---
+++ BistroCommon/src/main/java/Entities/WaitingList.java
@@ -0,0 +1,37 @@
+package Entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WaitingList {
+	private LocalDate waitingListDate;
+	private List<WaitingListEntry> waitingList;
+	
+	public WaitingList(LocalDate waitingListDate) {
+		this.waitingListDate = waitingListDate;
+		this.waitingList = new ArrayList<>();
+	}
+	
+	//getters
+	
+	public List<WaitingListEntry> getWaitingList(){
+		return this.waitingList;
+	}
+	
+	public LocalDate getWaitingListDate() {
+		return this.waitingListDate;
+	}
+	
+	public void addEntryToWaitingList(WaitingListEntry waitingListEntry) {
+		waitingList.add(waitingListEntry);
+	}
+	
+	/**
+	 * getCurrentWaitingList method returns the current waiting list
+	 * @return the current waiting list - all entries who's exit reason is still null
+	 */
+	public List<WaitingListEntry> getCurrentWaitingList(){
+		return this.waitingList.stream().filter(e -> e.getExitReason() == null).toList();
+	}
+}
