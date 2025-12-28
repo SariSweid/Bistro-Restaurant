7946d05a7d870bbe324980c81af200e1b8d31fec
yarin8294 <yarham1999@gmail.com>
2025-12-28 16:02:37 +0200

    added Subscriber entity

--- BistroCommon/src/main/java/Entities/Subscriber.java
+++ BistroCommon/src/main/java/Entities/Subscriber.java
@@ -1,5 +1,36 @@
 package Entities;
 
-public class Subscriber {
+import enums.UserRole;
 
+public class Subscriber extends User{
+	private String name;
+	private String userName;
+	private String membershipCode;
+	
+	public Subscriber(int userID, String name, String email, String phone, String userName, String membershipCode) {
+		super(userID, email, phone, UserRole.SUBSCRIBER);
+		this.name = name;
+		this.userName = userName;
+		this.membershipCode = membershipCode;
+	}
+	
+	//getters
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public String getMembershipCode() {
+		return this.membershipCode;
+	}
+	
+	//setters
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
 }
