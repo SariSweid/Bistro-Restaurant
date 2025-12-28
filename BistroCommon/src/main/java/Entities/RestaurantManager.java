dfebc529ddd5681ccfdcc674b7aee7271fc088e6
yarin8294 <yarham1999@gmail.com>
2025-12-28 16:06:12 +0200

    added RestaurantManager Entity

--- BistroCommon/src/main/java/Entities/RestaurantManager.java
+++ BistroCommon/src/main/java/Entities/RestaurantManager.java
@@ -1,5 +1,30 @@
 package Entities;
 
-public class RestaurantManager {
+import enums.UserRole;
 
+public class RestaurantManager extends User{
+	private String name;
+	private String userName;
+	
+	public RestaurantManager(int userID, String name, String email, String phone, String userName) {
+		super(userID, email, phone, UserRole.MANAGER);
+		this.name = name;
+		this.userName = userName;
+	}
+	
+	//getters
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	//setters
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
 }
