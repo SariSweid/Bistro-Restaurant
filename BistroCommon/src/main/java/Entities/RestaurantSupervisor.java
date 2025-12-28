879d86f0362ed624a403b4fd19739f7640b5fb9e
yarin8294 <yarham1999@gmail.com>
2025-12-28 16:05:14 +0200

    added RestaurantSupervisor Entity

--- BistroCommon/src/main/java/Entities/RestaurantSupervisor.java
+++ BistroCommon/src/main/java/Entities/RestaurantSupervisor.java
@@ -1,5 +1,30 @@
 package Entities;
 
-public class RestaurantSupervisor {
+import enums.UserRole;
 
+public class RestaurantSupervisor extends User{
+	private String name;
+	private String userName;
+	
+	public RestaurantSupervisor(int userID, String name, String email, String phone, String userName) {
+		super(userID, email, phone, UserRole.SUPERVISOR);
+		this.name = name;
+		this.userName = userName;
+	}
+	
+	//getters
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	//setters
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
 }
