424a71b30c793c9d59d5243efc419a5a273436e7
yarin8294 <yarham1999@gmail.com>
2026-01-01 19:51:33 +0200

    created Report entity

--- BistroCommon/src/main/java/Entities/Report.java
+++ BistroCommon/src/main/java/Entities/Report.java
@@ -1,5 +1,61 @@
 package Entities;
 
-public class Report {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import enums.ReportType;
+
+public class Report {
+	private int reportID;
+	private ReportType reportType;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private LocalDateTime generatedAt;
+	private String content;
+	
+	//constructor for new Report
+	public Report(int reportID, ReportType reportType, LocalDate startDate, LocalDate endDate, String content) {
+		this.reportID = reportID;
+		this.reportType = reportType;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.generatedAt = LocalDateTime.now();
+		this.content = content;
+	}
+	
+	//constructor for Report from db
+	public Report(int reportID, ReportType reportType, LocalDate startDate, LocalDate endDate, LocalDateTime generatedAt, String content) {
+		this.reportID = reportID;
+		this.reportType = reportType;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.generatedAt = LocalDateTime.now();
+		this.content = content;
+	}
+	
+	//getters
+	
+	public int getReportID() {
+		return this.reportID;
+	}
+	
+	public ReportType getReportType() {
+		return this.reportType;
+	}
+	
+	public LocalDate getStartDate() {
+		return this.startDate;
+	}
+	
+	public LocalDate getEndDate() {
+		return this.endDate;
+	}
+	
+	public LocalDateTime getGeneratedAt() {
+		return this.generatedAt;
+	}
+	
+	public String getContent() {
+		return this.content;
+	}
 }
