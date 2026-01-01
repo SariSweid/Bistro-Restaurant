eb433d022552e85c379771261bf06e27defaf2b0
yarin8294 <yarham1999@gmail.com>
2026-01-01 14:25:26 +0200

    created Bill entity

--- BistroCommon/src/main/java/Entities/Bill.java
+++ BistroCommon/src/main/java/Entities/Bill.java
@@ -1,5 +1,55 @@
 package Entities;
 
-public class Bill {
+import java.time.LocalDateTime;
 
+public class Bill {
+	private int billID;
+	private int reservationID;
+	private double totalAmount;
+	private LocalDateTime issuedAt;
+	private boolean paid;
+	
+	//constructor for new bill
+	public Bill(int billID, int reservationID, double totalAmount) {
+		this.billID = billID;
+		this.reservationID = reservationID;
+		this.totalAmount = totalAmount;
+		this.issuedAt = LocalDateTime.now();
+		this.paid = false;
+	}
+	
+	//constructor for bill from db
+	public Bill(int billID, int reservationID, double totalAmount, LocalDateTime issuedAt, boolean paid) {
+		this.billID = billID;
+		this.reservationID = reservationID;
+		this.totalAmount = totalAmount;
+		this.issuedAt = issuedAt;
+		this.paid = paid;
+	}
+	
+	//getters
+	
+	public int getBillID() {
+		return this.billID;
+	}
+	
+	public int getReservationID() {
+		return this.reservationID;
+	}
+	
+	public double getTotalAmount() {
+		return this.totalAmount;
+	}
+	
+	public LocalDateTime getIssuedAt() {
+		return this.issuedAt;
+	}
+	
+	public boolean isPaid() {
+		return this.paid;
+	}
+	
+	public void markAsPaid() {
+		this.paid = true;
+	}
 }
