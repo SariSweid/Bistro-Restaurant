ce33cb1c8de43a138c8683a86a0479c65d60f21d
yarin8294 <yarham1999@gmail.com>
2026-01-07 10:20:55 +0200

    finish javadoc for TableController

--- BistroServer/src/main/java/logicControllers/TableController.java
+++ BistroServer/src/main/java/logicControllers/TableController.java
@@ -20,7 +20,7 @@ public class TableController {
 	}
 	
 	/**
-	 * 
+	 * returns all the tables
 	 * @return all tables in the restaurant
 	 */
 	public List<Table> getAllTables(){
@@ -28,7 +28,7 @@ public class TableController {
 	}
 	
 	/**
-	 * 
+	 * finds a table by its id
 	 * @param tableID
 	 * @return table found by tableID
 	 */
@@ -36,6 +36,13 @@ public class TableController {
 		return this.dbController.GetTable(tableID);
 	}
 	
+	/**
+	 * finds the optimal available table for num of guests at given time and date
+	 * @param numOfGuests
+	 * @param date
+	 * @param time
+	 * @return the optimal available table
+	 */
 	public Optional<Table> findAvailableTable(int numOfGuests, LocalDate date, LocalTime time){
 		//Tables that are reserved at this date and time
 		List<Reservation> reservations = this.dbController.getReservationsAt(date, time);
@@ -43,12 +50,21 @@ public class TableController {
 		//all the restaurant tables
 		List<Table> allTables = this.getAllTables();
 		
-		//filter the restaurant tables by
+		//filter the restaurant tables by:
+		//t -> t.canSeat(numOfGuests) - is the table big enough for the given num of guests
+		//t -> reservations.stream().noneMatch(r -> r.getTableID() == t.getTableID()) - the table is not taken already
+		//min((t1, t2) -> Integer.compare(t1.getCapacity(), t2.getCapacity())) - after all the filtering of the tables, 
+		//choose the table with the minimum capacity that is still big enough to hold the num of guests
         return allTables.stream().filter(t -> t.canSeat(numOfGuests)).filter(t -> 
         		reservations.stream().noneMatch(r -> r.getTableID() == t.getTableID())).min((t1, t2) -> 
         		Integer.compare(t1.getCapacity(), t2.getCapacity()));
 	}
 	
+	/**
+	 * marks table as occupied and update its status in db
+	 * @param tableID
+	 * @return true if the update succeeded
+	 */
 	public boolean occupyTable(int tableID) {
 		Table table = this.getTableByID(tableID);
 		if(table == null || !table.isAvailable()) {
@@ -58,6 +74,11 @@ public class TableController {
 		return this.dbController.UpdateTable(table);
 	}
 	
+	/**
+	 * marks table as available and update its status in db
+	 * @param tableID
+	 * @return true if the update succeeded
+	 */
 	public boolean releaseTable(int tableID) {
 		Table table = this.getTableByID(tableID);
 		if(table == null || !table.isAvailable()) {
