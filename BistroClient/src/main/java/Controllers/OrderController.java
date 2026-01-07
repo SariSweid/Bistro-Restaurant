7798544324715ca36eaf6e7561408a17ee85d9f3
Leon Sigal <leonikoboy@gmail.com>
2026-01-07 16:40:21 +0200

    lkj

--- BistroClient/src/main/java/Controllers/OrderController.java
+++ BistroClient/src/main/java/Controllers/OrderController.java
@@ -15,47 +15,7 @@ public class OrderController {
     @FXML private TableColumn<?, ?> confirmationCodeColumn;
 
     
-    @FXML private CheckBox updateCheckBox;
-    @FXML private TextField dateField;
-    @FXML private TextField guestsField;
-    @FXML private TextField timeField;
-
     
-    @FXML private Button updateButton;
-
-    @FXML
-    private void initialize() {
-        // Make sure everything starts disabled
-        setUpdateControlsEnabled(false);
-
-        // dependant on checkbox
-        updateCheckBox.selectedProperty().addListener((obs, oldVal, isChecked) -> {
-            setUpdateControlsEnabled(isChecked);
-        });
-    }
-
-    private void setUpdateControlsEnabled(boolean enabled) {
-        dateField.setDisable(!enabled);
-        guestsField.setDisable(!enabled);
-        timeField.setDisable(!enabled);
-        updateButton.setDisable(!enabled);
-    }
-
-    
-    @FXML
-    private void onUpdateReservation() {
-        if (!updateCheckBox.isSelected()) {
-            return;
-        } 
-
-        String date = dateField.getText();
-        String guests = guestsField.getText();
-        String time = timeField.getText();
-        /*yet to connect to datbase and update there*/
-        
-        
-    }
-
     @FXML
     private void onPreviousPage() {
         SceneManager.switchTo("SubscriberUI.fxml");
--- BistroClient/src/main/java/Controllers/SubscriberInformationController.java
+++ BistroClient/src/main/java/Controllers/SubscriberInformationController.java
@@ -5,6 +5,9 @@ import util.SceneManager;
 
 public class SubscriberInformationController {
 	
+	
+	
+	
 	@FXML
 	private void onPreviousPage() {
 		SceneManager.switchTo("SupervisorUI.fxml");
